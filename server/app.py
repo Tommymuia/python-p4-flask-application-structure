2c9fd9d274d198dcaec07bac7f85728db552f2a1
Tommymuia <muiathomas.mt@gmail.com>
2025-12-15 14:56:55 +0300

    added

--- server/app.py
+++ server/app.py
@@ -1,5 +1,23 @@
 #!/usr/bin/env python3
-
 from flask import Flask
 
+
 app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return '<h1>Welcome to my page!<h1>'
+
+# append to server/app.py
+@app.route('/<username>')
+def user(username):
+    return f'<h1>Profile picture for {username}<h1>'
+
+# modify user() in server/app.py
+@app.route('/<string:username>')
+def user(username):
+    return f'Profile picture for {username}'
+
+if __name__ =='__main__':
+    app.run(port=5555,debug=True)
